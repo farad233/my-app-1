7cd366ec88eb173ccb96392d4b153581f8dabdae
farad233 <farad233@gmail.com>
2022-05-05 18:10:42 +0300

    sadas

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+export function Footer() {
+    return (
+        <div className="footer">
+            <a href="/">contact</a>
+            <a href="/">shop</a>
+            <a href="/">about ass</a>
+        </div>
+
+    );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export function Header() {
+    return (
+        <div className="nav">
+            <a href="/">menu</a>
+            <a href="/">shop</a>
+            <a href="/">about ass</a>
+        </div>
+
+    );
+}
